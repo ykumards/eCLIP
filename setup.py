eee0b58b27444ebbc05fd762cba37ffffe23ca06
yogesh kumar <ykumar@nyu.edu>
2024-07-08 13:27:41 +0300

    add requirements in setup.py

--- setup.py
+++ setup.py
@@ -5,9 +5,20 @@ setup(
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
-        "torch",
-        "transformers",
-        # other dependencies...
+        "datasets==2.15.0",
+        "hydra-core==1.3.0",
+        "numpy==1.26.2",
+        "pandas==2.1.3",
+        "peft==0.6.2",
+        "plotly==5.22.0",
+        "pytorch-lightning==2.1.2",
+        "rich==13.7.1",
+        "scikit-image==0.24.0",
+        "scikit-learn==1.3.2",
+        "torch==2.1.1",
+        "torchvision==0.16.1",
+        "transformers==4.35.2",
+        "wandb==0.16.0"
     ],
     author="Yogesh Kumar",
     author_email="ykumards@gmail.com",
